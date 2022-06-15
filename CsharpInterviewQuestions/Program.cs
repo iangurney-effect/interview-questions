2dcf3e397bd465b10c8eda6c087df66e338febb6
Ian Gurney <iangurney@effectphotonics.com>
2022-06-15 15:24:33 +0100

    Add project files.

---
+++ CsharpInterviewQuestions/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Question1();
+
+            Console.ReadKey();
+        }
+
+        static void Question1()
+        {
+            // Can you sort this array in ascending value.
+            int[] array = new int[] { 3, 4, 1, 5, 9, 2, 6, 0, 7, 8 };
+
+            // Add code here.
+            
+            // Display answer.
+            Console.WriteLine(string.Join(", ", array));
+        }
+
+        static void Question2()
+        {
+            // Can you write some code to differentiate y with respect to x for the following data set.
+            // Feel free to create your own class if you would like.
+            double[] y = new double[] { 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5 };
+            double[] x = new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // Add code here.
+            double[] dydx = new double[0];
+
+            // Display answer.
+            Console.WriteLine(string.Join(", ", dydx));
+        }
+
+        static void Question3()
+        {
+            // Can you write some code to find value in x corresponding to the peak of the second differential in y with respect to x.
+            // Feel free to create your own class if you would like.
+            double[] y = new double[] { 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5 };
+            double[] x = new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // Add code here.
+            double peak = double.NaN;
+
+            // Display answer.
+            Console.WriteLine(peak);
+        }
+    }
+}
---
+++ PythonInterviewQuestions/Main.py
@@ -0,0 +1,34 @@
+def question1():
+    # Can you sort this array in ascending value.
+    array = [3, 4, 1, 5, 9, 2, 6, 0, 7, 8]
+
+    # Add code here.
+
+    # Display answer.
+    print(array)
+
+def question2():
+    # Can you write some code to differentiate y with respect to x for the following data set.
+    # Feel free to create your own class if you would like.
+    y = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0]
+    x = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+
+    # Add code here.
+    dydx = None;
+
+    # Display answer.
+    print(dydx);
+
+def question3():
+    # Can you write some code to find value in x corresponding to the peak of the second differential in y with respect to x.
+    # Feel free to create your own class if you would like.
+    y = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0]
+    x = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+
+    # Add code here.
+    peak = None;
+
+    # Display answer.
+    print(peak);
+
+question1()
