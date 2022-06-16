8e306e036c93e2d6b23aa886e8d52abe5ba2d5a5
Ian Gurney <iangurney@effectphotonics.com>
2022-06-16 11:30:08 +0100

    Add C# unit tests

---
+++ CsharpUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CsharpUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EFFECT Photonics")]
+[assembly: AssemblyProduct("CsharpUnitTests")]
+[assembly: AssemblyCopyright("Copyright © EFFECT Photonics 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("93c4cbaa-cf4a-4993-982e-369a651ef08d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CsharpUnitTests/UnitTests.cs
@@ -0,0 +1,37 @@
+﻿using InterviewQuestions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CsharpUnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void Test_Answer1()
+        {
+            int[] expected = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] actual = Questions.Answer1(new int[] { 3, 4, 1, 5, 9, 2, 6, 0, 7, 8 });
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Answer2()
+        {
+            double[] expected = new double[] { 0, 0, 0, 0, 0, 1, 1, 1, 1, 1 };
+            double[] actual = Questions.Answer2(new double[] { 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5 }, new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Answer3()
+        {
+            double expected = 5;
+            double actual = Questions.Answer3(new double[] { 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5 }, new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
