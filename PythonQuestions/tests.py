e809de9c7da01c7cab54adf42bf908837a37039d
Ian Gurney <iangurney@effectphotonics.com>
2022-06-16 11:32:05 +0100

    Make code more readable

--- PythonQuestions/tests.py
+++ PythonQuestions/tests.py
@@ -2,6 +2,7 @@ import unittest
 from Main import *
 
 class Test_answers(unittest.TestCase):
+
     def test_answer1(self):
         expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         actual = answer1([3, 4, 1, 5, 9, 2, 6, 0, 7, 8])
